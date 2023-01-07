eb9c0643582f29a991d3bfa682ef56a0d08c4b4c
djcoully7@gmail.com <djcoully7@gmail.com>
2023-01-07 01:36:58 +0000

    VIDeO

--- ajax_sample.js
+++ ajax_sample.js
@@ -1,37 +1,25 @@
-let index = 0;
-let data = []; // Ajout d'une variable pour stocker les données extraites de ajax.json
-const button = document.getElementById('btn');
+let number = 0;
+const videoArea = document.getElementById("video");
 const titleArea = document.getElementById("title");
 const contentArea = document.getElementById("content");
-const videoArea = document.getElementById("video");
+const button = document.getElementById('btn');
 
 function getData() {
-
-  // Décrire le processus pour obtenir des données depuis ajax.json
-  const request = new XMLHttpRequest();
-  request.open ("GET", "ajax.json");
-  request.responseType = "json";
-  request.send(null);
-  request.onreadystatechange = function(){
-    if (request.readyState==4){
-      if (request.status==200){
-        data = request.response;
+  button.addEventListener('click', e => {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4) {
+        if(request.status == 200) {
+          titleArea.innerHTML = request.response[number].title;
+          contentArea.innerHTML = request.response[number].content;
+          videoArea.setAttribute("src", request.response[number].url);
+          number == 2 ? number = 0 : number++;
+        }
       }
     }
-  }
-};
-
-function changeVideo() {
-  // Décrivez le traitement lorsque le bouton est cliqué
-  // Appelez le processus getData uniquement si vous n'avez pas les données d'ajax.json
-  if (!data.length)getData();
-  button.onclick = function(){
-    titleArea.innerHTML= data[index].title;
-    contentArea.innerHTML= data[index].content;
-    videoArea.setAttribute("src", data[indesx].url);
-    index++;
-    if (index==data.length) index =0
-  };
+    request.open("GET", "ajax.json");
+    request.responseType = "json";
+    request.send(null);
+  })
 }
-
-window.onload = changeVideo;
+window.onload = getData;
